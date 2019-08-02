cae674808d2468885ab020be0a77315967454847
CarlosEscat <carlosescat@gmail.com>
2019-08-02 11:59:30 +0200

    image

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const db = require("./db");
+const Image = require("./image/model");
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
